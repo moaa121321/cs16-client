0f2f37a5e7b9accc7b0764eb59efc4cbe8be749a
moaa121321 <agsakkalnico@gmail.com>
2025-09-05 14:20:26 +0400

    Update NoRecoil.cpp

--- cl_dll/NoRecoil.cpp
+++ cl_dll/NoRecoil.cpp
@@ -1,1 +1,17 @@
+// NoRecoil.h
+#ifndef __NORECOIL_H__
+#define __NORECOIL_H__
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern cvar_t *cl_norecoil;
+void ApplyNoRecoil(float frametime, float *punchangle, float *viewangle);
+void NoRecoil_Init(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
